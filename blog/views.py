e8dc55b44938fdeb539710c6a4599e3876bd4223
Aleksandr <smirnow-2012@mail.ru>
2023-01-29 02:45:15 +0300

    update blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blog.models import Post
 from django.contrib.auth.models import User
 
 
 class UserPostListView(ListView):
     model = Post
-    context_object_name = 'posts_list'
     template_name = 'blog/user_posts.html'
 
-    def get_queryset(self):
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
         user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).order_by('-date_created')
+        context['author_posts'] = get_object_or_404(User, username=self.kwargs.get('username'))
+        context['posts_list'] = Post.objects.filter(author=user).order_by('-date_created')
+        return context
